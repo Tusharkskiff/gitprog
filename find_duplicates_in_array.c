615b95e8f928e9a153d431ac88f90836c90f8d36
Tusharkskiff <k.tushar@skiff.com>
2023-09-21 19:02:12 +0530

    Update find_duplicates_in_array.c

--- find_duplicates_in_array.c
+++ find_duplicates_in_array.c
@@ -1,8 +1,38 @@
 //Program to find the duplicates in the array
+
+/*
+
+output:
+
+Enter the length of array : 5
+Enter the elements of the array : 1 1 2 2 3 
+Duplicate pair 1 and the value is 1 and 1
+Duplicate pair 2 and the value is 2 and 2
+There are 2 duplicates in the array
+
+*/
+
+
 #include<stdio.h>
 
-int duplicatechecker(int arr[],int length);//function prototype
+//Function to find the duplicates
+int duplicatechecker(int arr[],int length)
+{
+    int count=0;
+    for (int i=0;i<length;i++)
+    {
+        for (int j=0;j<length;j++)
+            if (j>i)
+                if (arr[i]==arr[j])
+                {
+                    count++;
+                    printf("\nDuplicate pair %d and the value is %d and %d",count,arr[i],arr[j]);
+                }
+    }
+    return count;
+}
 
+//main function
 int main()
 {
     int arr[100],length,count;
@@ -24,27 +54,4 @@ int main()
         printf("\nThere are no duplicates in the array");
 
     return 0;
-}
-
-
-//Function tom find the duplicates
-
-int duplicatechecker(int arr[],int length)
-{
-    int count=0;
-    for (int i=0;i<length;i++)
-    {
-        for (int j=0;j<length;j++)
-        {
-            if (j>i)
-            {
-                if (arr[i]==arr[j])
-                {
-                    count++;
-                    printf("\nDuplicate pair %d and the value is %d and %d",count,arr[i],arr[j]);
-                }
-            }
-        }
-    }
-    return count;
 }
